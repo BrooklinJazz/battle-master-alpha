c66002dd20e402b085e0f3c176d5d8cff001f734
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-01-31 12:22:35 -0800

    create ClearCombatant.js component. rename app.js to App.js

---
+++ src/containers/ClearCombatant.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ClearCombatant extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <button className="btn btn-default">Clear Combatants</button>
+    )
+  }
+}
+
+export default ClearCombatant
